3c19bce40110037c4045bef819c301b9002b4d26
Medina Daci <diname_girl@hotmail.com>
2021-02-23 19:34:07 +0100

    Initial commit.

---
+++ SecurityVulnerabilities/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SecurityVulnerabilities.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
